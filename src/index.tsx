ccbd72079dd7a329a7bd110a1f3f5daef51dcf4c
Rafael <Rafabatera-8@hotmail.com>
2021-08-20 16:03:40 -0300

    global variables. styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/global-styles.scss';
+import './global/global-styles.scss';
 import { Home } from './templates/Home';
 
 
